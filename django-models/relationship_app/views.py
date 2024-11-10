0101d65a7319d8e4dfd4ccb82e258463374481eb
Muluken <webruselitex41@gmail.com>
2024-11-11 00:19:55 +0300

    Implemented RBAC

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -10,6 +10,45 @@ from django.contrib.auth import login
 from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from .models import Book
+from .forms import BookForm  # Assuming you have a BookForm for adding/editing books
+
+# View for adding a book
+@permission_required('relationship_app.can_add_book', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')  # Redirect to the book list or a confirmation page
+    else:
+        form = BookForm()
+    return render(request, 'add_book.html', {'form': form})
+
+# View for editing a book
+@permission_required('relationship_app.can_change_book', raise_exception=True)
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')  # Redirect to the book list
+    else:
+        form = BookForm(instance=book)
+    return render(request, 'edit_book.html', {'form': form})
+
+# View for deleting a book
+@permission_required('relationship_app.can_delete_book', raise_exception=True)
+def delete_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('book_list')  # Redirect to the book list
+    return render(request, 'delete_book.html', {'book': book})
+
 
 # Helper function to check if the user has the required role
 def role_required(role):
