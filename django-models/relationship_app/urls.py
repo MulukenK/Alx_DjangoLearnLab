d8de8c52e0c524fb2356ba87c81206f57191f9dd
Muluken <webruselitex41@gmail.com>
2024-11-10 23:57:17 +0300

    User Authentication

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'), 
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),  
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(template_name=", "LoginView.as_view(template_name=, name='logout'),
     path('register/', views.register, name='register'),
+    
 ]
