039d4d944c1986c3c4641a1c65ee08bec8ac83bc
Muluken <webruselitex41@gmail.com>
2024-11-11 00:17:46 +0300

    Implemented custom permissions

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -4,6 +4,23 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    publication_date = models.DateField()
+    
+    # Custom permissions
+    class Meta:
+        permissions = [
+            ('can_add_book', 'Can add book'),
+            ('can_change_book', 'Can change book'),
+            ('can_delete_book', 'Can delete book'),
+        ]
+
+    def __str__(self):
+        return self.title
+
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
