91007798fa8e27df5d7247ac460cc1fa3c2520a1
Muluken <webruselitex41@gmail.com>
2024-11-10 23:17:12 +0300

    minor name change

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -8,13 +8,13 @@ for book in books_by_author:
     print(book.title)
 
 
-library = Library.objects.get(name="library_name")
+library = Library.objects.get(name=library_name)
 books_in_library = library.books.all() 
 print("\nBooks in library_name:")
 for book in books_in_library:
     print(book.title)
 
 
-library = Library.objects.get(name="library_name")
+library = Library.objects.get(name=library_name)
 librarian_for_library = library.librarian  
 print(f"\nLibrarian for Central Library: {librarian_for_library.name}")
