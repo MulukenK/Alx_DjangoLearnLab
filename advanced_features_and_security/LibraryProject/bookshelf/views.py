6edfad9a164d8e0255f02319f98246e48d6b4aa1
Muluken <webruselitex41@gmail.com>
2024-11-17 17:02:10 +0300

    Implemented permissions and groups

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -5,30 +5,17 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import YourModel
+from .models import MetaModel
+from .models import Book
 
 @permission_required('yourapp.can_view', raise_exception=True)
-def view_instance(request, instance_id):
-    instance = YourModel.objects.get(id=instance_id)
-    return render(request, 'yourapp/view_instance.html', {'instance': instance})
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'yourapp/book_list.html', {'books': books})
 
 @permission_required('yourapp.can_create', raise_exception=True)
-def create_instance(request):
+def books(request):
     if request.method == "POST":
-        # Code to create a new instance
-        return redirect('view_instance', instance_id=new_instance.id)
-    return render(request, 'yourapp/create_instance.html')
-
-@permission_required('yourapp.can_edit', raise_exception=True)
-def edit_instance(request, instance_id):
-    instance = YourModel.objects.get(id=instance_id)
-    if request.method == "POST":
-        # Code to edit instance
-        return redirect('view_instance', instance_id=instance.id)
-    return render(request, 'yourapp/edit_instance.html', {'instance': instance})
-
-@permission_required('yourapp.can_delete', raise_exception=True)
-def delete_instance(request, instance_id):
-    instance = YourModel.objects.get(id=instance_id)
-    instance.delete()
-    return HttpResponse("Instance deleted")
+        # Code to add a new book entry
+        return redirect('book_list')
+    return render(request, 'yourapp/books.html')
