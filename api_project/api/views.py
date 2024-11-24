5e76d4edcc90516891e0160666f12c2d12fe3e9f
Muluken <webruselitex41@gmail.com>
2024-11-24 22:17:12 +0300

    Setup Authentication

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  
@@ -10,3 +11,14 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all() 
     serializer_class = BookSerializer  
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  
+
+    def get_permissions(self):
+        """Grant admin users full access, others can only list and retrieve"""
+        if self.action in ['create', 'update', 'destroy']:
+            return [IsAdminUser()]
+        return [IsAuthenticated()]  
