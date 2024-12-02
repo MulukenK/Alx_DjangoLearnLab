ac35723479f678f3d8d8f875ca2c2ab3b91867f8
Muluken <webruselitex41@gmail.com>
2024-12-02 12:07:47 +0300

    Setup django envt and added templates and static files

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -75,9 +75,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'Mule',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
