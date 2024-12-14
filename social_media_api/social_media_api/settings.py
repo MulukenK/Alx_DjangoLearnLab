00f0caeb9ff9f9aa82fa39860c07c9b441fd47f0
Muluken Kefale <mulek@Mulukens-MacBook-Air.local>
2024-12-15 00:15:07 +0300

    Production

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h(l$-dazzoo5kxpgt17=z7ul8_#oorntsbh*lm&vt6&(bw0n_b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
